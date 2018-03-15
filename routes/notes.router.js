b61c5b3e33602fa4ae5fae8e83448ac91c48b1be
George Colon <geocolon@gmail.com>
2018-03-15 11:57:43 -0700

    first commit

---
+++ routes/notes.router.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const express = require('express');
+const knex = require('../knex');
+
+const router = express.Router();
+
+/* ========== GET/READ ALL NOTES ========== */
+router.get('/notes', (req, res, next) => {
+  const searchTerm = req.query.searchTerm;
+  const folderId = req.query.folderId;
+
+  knex.select('notes.id', 'title', 'content', 'folders.id as folder_id', 'folders.name as folderName')
+    .from('notes')
+    .leftJoin('folders', 'notes.folder_id', 'folders.id')
+    .modify(function (queryBuilder) {
+      if (searchTerm) {
+        queryBuilder.where('title', 'like', `%${searchTerm}%`);
+      }
+    })
+    .modify(function (queryBuilder) {
+      if (folderId) {
+        queryBuilder.where('folder_id', folderId);
+      }
+    })
+    .orderBy('notes.id')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+
+/* ========== GET/READ SINGLE NOTES ========== */
+router.get('/notes/:id', (req, res, next) => {
+  const noteId = req.params.id;
+
+  knex.first('notes.id', 'title', 'content',
+    'folders.id as folder_id', 'folders.name as folderName')
+    .from('notes')
+    .leftJoin('folders', 'notes.folder_id', 'folders.id')
+    .where('notes.id', noteId)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
+});
+
+/* ========== POST/CREATE ITEM ========== */
+router.post('/notes', (req, res, next) => {
+  const { title, content, folder_id } = req.body;
+
+  /***** Never trust users. Validate input *****/
+  if (!req.body.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = {
+    title: title,
+    content: content,
+    folder_id: folder_id  // Add `folder_id`
+  };
+
+  let noteId;
+  // Insert new note, instead of returning all the fields, just return the new `id`
+  knex.insert(newItem)
+    .into('notes')
+    .returning('id')
+    .then(([id]) => {
+      noteId = id;
+      // Using the new id, select the new note and the folder
+      return knex.select('notes.id', 'title', 'content', 'folder_id', 'folders.name as folder_name')
+        .from('notes')
+        .leftJoin('folders', 'notes.folder_id', 'folders.id')
+        .where('notes.id', noteId);
+    })
+    .then((results) => {
+      const result = results[0];
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+
+});
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/notes/:id', (req, res, next) => {
+  const noteId = req.params.id;
+  const { title, content, folder_id } = req.body;
+
+  /***** Never trust users. Validate input *****/
+  if (!req.body.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateItem = {
+    title: title,
+    content: content,
+    folder_id: folder_id
+  };
+
+  knex('notes')
+    .update(updateItem)
+    .where('id', noteId)
+    .returning(['id'])
+    .then(() => {
+      // Using the noteId, select the note and the folder info
+      return knex.select('notes.id', 'title', 'content', 'folder_id', 'folders.name as folder_name')
+        .from('notes')
+        .leftJoin('folders', 'notes.folder_id', 'folders.id')
+        .where('notes.id', noteId);
+    })
+    .then(([result]) => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
+});
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/notes/:id', (req, res, next) => {
+  knex.del()
+    .where('id', req.params.id)
+    .from('notes')
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
